7d91db8d26db7512d611fbf462297d58fc5d204d
Vincent Reynaud <vincentreynaud.de@gmail.com>
2021-12-20 15:06:54 +0100

    setup first contentful query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,15 @@
 import { graphql } from 'gatsby';
 import * as React from 'react';
 import Layout from '../components/Layout';
+import { renderRichText } from 'gatsby-source-contentful/rich-text';
 
 export default function Index({ data }) {
-  const { site } = data;
+  const { site, about } = data;
+
+  const [bla] = about.edges.map(({ node }) => node);
   return (
     <Layout siteData={site.siteMetadata}>
-      <section id='about'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam rerum est facilis distinctio
-        officia possimus! Velit quae quidem repellendus iste veniam voluptatum porro itaque incidunt
-        totam, non corrupti aliquid voluptates quaerat tempora, excepturi dolore vel, blanditiis
-        quasi placeat asperiores! Facere veritatis distinctio atque tempora soluta doloribus. Quae
-        culpa perspiciatis nihil possimus fuga qui ducimus consequuntur quaerat odit repellat. Sunt
-        expedita iusto libero exercitationem? Neque a nemo voluptates harum tempore debitis
-        quibusdam laboriosam fugiat exercitationem? Possimus quisquam dolorum sequi ad nemo quos
-        deleniti incidunt, laborum, aliquam quis natus fugit unde totam expedita quas quidem
-        aperiam, temporibus fuga dicta corrupti recusandae quibusdam vero atque mollitia! Soluta aut
-        modi inventore voluptatibus dignissimos reiciendis aspernatur dolorum expedita aperiam
-        facere. Voluptates possimus quaerat rerum labore!
-      </section>
+      <section id='about'>{renderRichText(bla.bio)}</section>
     </Layout>
   );
 }
@@ -33,6 +24,22 @@ export const query = graphql`
         links {
           name
           href
+        }
+      }
+    }
+    about: allContentfulAbout {
+      edges {
+        node {
+          bio {
+            raw
+          }
+          contentful_id
+          education {
+            raw
+          }
+          portrait {
+            gatsbyImageData
+          }
         }
       }
     }
