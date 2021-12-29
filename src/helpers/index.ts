30e5117692738a6542462f5e5642b331552bf397
vincentreynaud <vincentreynaud.de@gmail.com>
2021-12-29 14:07:44 +0100

    add auto link injection to privacy policy

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,3 +6,10 @@ export function formatProjectDate(date: string) {
 export function formatProjectTimeframe(start: string, end: string) {
   return formatProjectDate(start) + ' - ' + formatProjectDate(end);
 }
+
+// TODO: add html obfuscation
+type EmbedHtmlAnchorOpts = { targetBlank: boolean; displayStr?: string };
+export function embedHtmlAnchor(href: string, opts: EmbedHtmlAnchorOpts = { targetBlank: true }) {
+  const targetStr = "target='_blank' rel='noreferrer noopener'";
+  return `<a href='${href}' ${opts?.targetBlank && targetStr}>${opts.displayStr ? opts.displayStr : href}</a>`;
+}
--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,17 +1,23 @@
 import { graphql } from 'gatsby';
+import { find } from 'lodash';
 import * as React from 'react';
 import Layout from '../components/Layout';
+import { embedHtmlAnchor } from '../helpers';
 
 export default function Privacy({ data }) {
-  const { content } = data;
+  const { content, site } = data;
   const [privacy] = content.edges.map(({ node }) => node.privacyPolicy.childMarkdownRemark);
+  const email = find(site.siteMetadata.links, (link) => link.name.toLowerCase() === 'email');
+  const privacyHtml = privacy.html
+    .replace(/\{email\}/gm, embedHtmlAnchor(email.href))
+    .replace(/\{siteUrl\}/gm, embedHtmlAnchor(site.siteMetadata.url));
 
   return (
     <Layout seo={{ title: 'Privacy Policy' }}>
       <article
         id='privacy'
         dangerouslySetInnerHTML={{
-          __html: privacy.html,
+          __html: privacyHtml,
         }}
       ></article>
     </Layout>
@@ -20,6 +26,15 @@ export default function Privacy({ data }) {
 
 export const query = graphql`
   query {
+    site {
+      siteMetadata {
+        url
+        links {
+          name
+          href
+        }
+      }
+    }
     content: allContentfulImprintPrivacyPolicy {
       edges {
         node {
