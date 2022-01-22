5f947a29228b111be8d48a0346f49e7fd0f34d52
vincentreynaud <vincentreynaud.de@gmail.com>
2022-01-22 12:19:31 +0100

    add links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-export default function Footer({ title }) {
+export default function Footer({ title, links }) {
   return (
-    <footer
-      id='footer'
-      className='d-flex flex-column flex-md-row justify-content-center justify-content-md-between align-items-center p-0'
-    >
-      <small className='legal'>
-        <nav className='d-inline'>
-          <Link to='/imprint'>Imprint</Link> &middot; <Link to='/privacy-policy'>Privacy Policy</Link>&nbsp;
-        </nav>
-        <span>&copy; {new Date().getFullYear()}&nbsp;</span>
-        <span className='text-nowrap'>{title}</span>
+    <footer id='footer'>
+      <small>
+        <ul className='list-unstyled ms-0 mb-0 d-flex flex-row justify-content-start'>
+          {links.map((link) => (
+            <li className='me-2 mb-1'>
+              <a href={link.href}>{link.name}</a>
+            </li>
+          ))}
+        </ul>
       </small>
-      <small className='credits'>
-        Website by&nbsp;
-        <a href='https://vincentreynaud.de' target='_blank' rel='noopener noreferrer'>
-          Vincent Reynaud
-        </a>
-      </small>
+      <div className='d-flex flex-column flex-sm-row justify-content-start justify-content-sm-between align-items-start p-0'>
+        <small className='legal mb-1'>
+          <nav className='d-inline'>
+            <Link to='/imprint'>Imprint</Link> &middot; <Link to='/privacy-policy'>Privacy Policy</Link>&nbsp;
+          </nav>
+          <span>&copy; {new Date().getFullYear()}&nbsp;</span>
+          <span className='text-nowrap'>{title}</span>
+        </small>
+        <small className='credits'>
+          Website by&nbsp;
+          <a href='https://vincentreynaud.de' target='_blank' rel='noopener noreferrer'>
+            Vincent Reynaud
+          </a>
+        </small>
+      </div>
     </footer>
   );
 }
