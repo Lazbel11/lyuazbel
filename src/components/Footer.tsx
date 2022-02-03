4841a35d330a2a1fc16c42895b1b070bb15c0779
Vincent Reynaud <vincentreynaud.de@gmail.com>
2022-02-03 14:42:51 +0100

    final footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,34 +4,10 @@ import { mapLinkIdToIcon } from '../config';
 import classNames from 'classnames';
 
 export default function Footer({ title, links, location }) {
-  const isLinkIcons = location?.pathname !== '/';
   return (
     <footer id='footer'>
-      <small className='d-flex flex-column flex-md-row justify-content-center justify-content-md-between align-items-center align-items-baseline p-0'>
-        <ul className='list-unstyled ms-0 mb-0 d-flex flex-row justify-content-start align-items-baseline'>
-          {links.map((link) => {
-            const Icon = mapLinkIdToIcon[link.name.toLowerCase()];
-            return (
-              <li
-                className={classNames('mb-1', {
-                  'me-2': !isLinkIcons,
-                  'me-4': isLinkIcons,
-                })}
-                key={link.name}
-              >
-                <a
-                  className={classNames({ iconLink: isLinkIcons })}
-                  href={link.href}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {isLinkIcons ? <Icon /> : link.name}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        <ul className='legal list-unstyled ms-0 mb-0 d-flex flex-row mb-1'>
+      <small className='d-flex flex-column flex-md-row justify-content-center justify-content-md-start align-items-center align-items-baseline p-0'>
+        <ul className='legal list-unstyled d-flex flex-row ms-0 mb-1 order-1'>
           <li className='me-2 mb-0'>
             <Link to='/imprint'>Imprint</Link>
           </li>
@@ -41,12 +17,31 @@ export default function Footer({ title, links, location }) {
           <span>&copy; {new Date().getFullYear()}&nbsp;</span>
           <span className='text-nowrap'>{title}</span>
         </ul>
-        <span className='credits'>
+        <span className='separator mx-2 order-2 mb-1'>&middot;</span>
+        <span className='credits order-3 mb-1'>
           Website by&nbsp;
           <a href='https://vincentreynaud.de' target='_blank' rel='noopener noreferrer'>
             Vincent Reynaud
           </a>
         </span>
+        <span className='separator mx-2 order-4 mb-1'>&middot;</span>
+        <ul className='contact list-unstyled d-flex flex-row justify-content-start align-items-baseline order-0 order-md-5 ms-0 mb-1'>
+          {links.map((link) => {
+            const Icon = mapLinkIdToIcon[link.name.toLowerCase()];
+            return (
+              <li className='mb-0 me-3' key={link.name}>
+                <a
+                  className={`iconLink ${link.name.toLowerCase()}`}
+                  href={link.href}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <Icon />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </small>
     </footer>
   );
